242bb04e4aa2836d321811c4f781f1093e87fc4a
123-SHIVA <shivayojha8@gmail.com>
2024-07-22 18:12:39 +0530

    my first commit

---
+++ Array/Sum.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Sum {
+
+
+
+     public static int sumArray(int arr[],int sum){
+
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];  
+        }
+
+        return sum;
+     }
+
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+         int arr[]=new int[n];
+        for(int i=0;i<arr.length;i++){
+            arr[i]=sc.nextInt();  
+        }
+
+        int sum=0;
+        int res=sumArray(arr, sum);
+        System.out.println(res);
+
+
+        
+
+    }
+}
